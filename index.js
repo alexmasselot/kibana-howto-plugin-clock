3e2268d10f087b614e7b33990fcb7b4298338563
Alexandre Masselot <alexandre.masselot@gmail.com>
2016-05-19 15:56:21 +0200

    init

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-import exampleRoute from './server/routes/example';
 
-export default function (kibana) {
+module.exports = function(kibana) {
   return new kibana.Plugin({
-    require: ['elasticsearch'],
-
-    uiExports: {
-      app: {
-        title: 'Dummy Viz',
-        description: 'An awesome Kibana plugin',
-        main: 'plugins/dummy_viz/app'
-      },
-      hacks: [
-        'plugins/dummy_viz/hack'
-      ]
-    },
-
-    config(Joi) {
-      return Joi.object({
-        enabled: Joi.boolean().default(true),
-      }).default();
-    },
-
-    init(server, options) {
-      // Add server routes and initalize the plugin here
-      exampleRoute(server);
-    }
-
+     uiExports: {
+        visTypes: [
+          'plugins/dummy_viz/clock'
+        ]
+      }
   });
 };
